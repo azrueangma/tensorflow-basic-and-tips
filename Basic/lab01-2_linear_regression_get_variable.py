5f981509aeb7cb4dba389fdd657c32a4d78731eb
Gyeonghoon Lee <azureangma@naver.com>
2018-05-11 18:21:55 +0900

    Update lab01-2_linear_regression_get_variable.py

--- Basic/lab01-2_linear_regression_get_variable.py
+++ Basic/lab01-2_linear_regression_get_variable.py
@@ -22,9 +22,6 @@ hypothesis = tf.nn.bias_add(tf.multiply(X, W), b, name = 'hypothesis')
 loss = tf.reduce_mean(tf.square(Y-hypothesis), name = 'loss')
 optim = tf.train.GradientDescentOptimizer(learning_rate = 0.001).minimize(loss)
 
-plt.figure(num=None, figsize=(8, 14), dpi=60, facecolor='w', edgecolor='k')
-plt.subplots_adjust(hspace = 0.4, top = 0.9, bottom = 0.05)
-
 init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init_op)
@@ -34,20 +31,7 @@ with tf.Session() as sess:
         l, W_value, b_value, _ = sess.run([loss, W, b, optim], feed_dict={X: dataX, Y: dataY})
         if (epoch+1) %100 == 0:
             print("Epoch [{:3d}/{:3d}], loss = {:.6f}".format(epoch + 1, TOTAL_EPOCH, l))
-
-            #for plot graph
-            arg_+=1
-            plt.subplot(5, 2, arg_)
-            plt.scatter(dataX, dataY, marker='.')
-            plt.plot(dataX, dataX * W_value + b_value, c='r')
-            plt.title('Epoch {}'.format(epoch+1))
-            plt.grid()
-            plt.xlim(-2, 2)
-
-plt.suptitle('LinearRegression', fontsize=20)
-plt.savefig('./image/LAB01-2_linear_regression_get_variable.jpg')
-plt.show()
-
+            
 '''
 Epoch [100/1000], loss = 0.174033
 Epoch [200/1000], loss = 0.118036
