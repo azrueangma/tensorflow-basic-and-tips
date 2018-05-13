6dda6a218d27e11c11a12decbbd140919871819e
Gyeonghoon Lee <azureangma@naver.com>
2018-05-13 21:58:12 +0900

    Upload lab02-1_tensorboard_basic_usages.py

---
+++ Basic/lab02-1_tensorboard_basic_usages.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import shutil
+import os
+
+BOARD_PATH = './board/lab02_1_board'
+
+a = tf.constant(value = 3, dtype = tf.float32, shape = [], name = 'a')
+b = tf.constant(value = 5, dtype = tf.float32, shape = [], name = 'b')
+c = tf.add(a,b, name = 'c')
+
+
+if os.path.exists(BOARD_PATH):
+    shutil.rmtree(BOARD_PATH)
+writer = tf.summary.FileWriter(BOARD_PATH)
+
+with tf.Session() as sess:
+    writer.add_graph(sess.graph)
+    print(sess.run(c))
+
+'''
+tensorboard --logdir=lab02_1_board
+
+'''
