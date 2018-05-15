970691c8f7a90e71c334cc5e90f72cc0b4dc2b25
Gyeonghoon Lee <azureangma@naver.com>
2018-05-16 00:43:32 +0900

    Update load_data.py

--- Basic/load_data.py
+++ Basic/load_data.py
@@ -15,3 +15,21 @@ def generate_data_for_linear_regression(npoints):
     trainX = np.expand_dims(vectors[:, 0], axis=1)
     trainY = np.expand_dims(vectors[:, 1], axis=1)
     return trainX, trainY
+
+def generate_data_for_two_class_classification(npoints):
+    for i in range(npoints):
+        np.random.seed(i)
+        x1 = np.random.normal(0.0, 0.5)
+        x2 = np.random.normal(2.0, 0.5)
+        tmp1 = np.expand_dims(np.array([x1, 0]), axis=0)
+        tmp2 = np.expand_dims(np.array([x2, 1]), axis=0)
+        if i == 0:
+            vectors = tmp1
+            vectors = np.append(vectors, tmp2, axis=0)
+        else:
+            vectors = np.append(vectors, tmp1, axis=0)
+            vectors = np.append(vectors, tmp2, axis=0)
+
+    trainX = np.expand_dims(vectors[:, 0], axis=1)
+    trainY = np.expand_dims(vectors[:, 1], axis=1)
+    return trainX, trainY.astype(int)
