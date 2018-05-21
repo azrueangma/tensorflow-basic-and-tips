7b8566fcedf4f329813c5db8f55bdeb14a85fef6
Gyeonghoon Lee <azureangma@naver.com>
2018-05-21 17:39:31 +0900

    Update lab04_6_data_manipulation_tfrecord_read.py

--- Basic/lab04_6_data_manipulation_tfrecord_read.py
+++ Basic/lab04_6_data_manipulation_tfrecord_read.py
@@ -21,6 +21,24 @@ with tf.Session() as sess:
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
     x_data, y_data = sess.run([feature_value, label_value])
+    print(x_data)
+    print(y_data)
     coord.request_stop()
     coord.join(threads)
-
+    
+'''
+[[0.   0.96 0.52 ... 0.86 0.39 1.  ]
+[0.   0.65 0.43 ... 0.07 1.   0.08]
+[0.37 0.71 0.79 ... 0.37 0.   0.22]
+...
+[0.98 0.89 0.48 ... 0.   0.   0.19]
+[0.3  0.97 0.81 ... 0.45 0.   0.69]
+[0.   1.   0.15 ... 0.4  0.   0.35]]
+[[7]
+ [2]
+ [0]
+ ...
+ [9]
+ [0]
+ [6]]
+'''
