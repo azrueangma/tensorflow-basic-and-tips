565c15e0ef2d4a146e6c77d432130a8340b8bc73
Gyeonghoon Lee <azureangma@naver.com>
2018-05-25 14:54:56 +0900

    Update lab04_6_data_manipulation_tfrecord_read.py

--- Basic/lab04_6_data_manipulation_tfrecord_read.py
+++ Basic/lab04_6_data_manipulation_tfrecord_read.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import numpy as np
 
 tfrecords_filename = "D:/STUDY/2018/MYT/tensorflow-basic-and-tips/Basics/data/pendigits.tfrecords"
 filename_queue = tf.train.string_input_producer([tfrecords_filename])
@@ -25,20 +24,3 @@ with tf.Session() as sess:
     print(y_data)
     coord.request_stop()
     coord.join(threads)
-    
-'''
-[[0.   0.96 0.52 ... 0.86 0.39 1.  ]
-[0.   0.65 0.43 ... 0.07 1.   0.08]
-[0.37 0.71 0.79 ... 0.37 0.   0.22]
-...
-[0.98 0.89 0.48 ... 0.   0.   0.19]
-[0.3  0.97 0.81 ... 0.45 0.   0.69]
-[0.   1.   0.15 ... 0.4  0.   0.35]]
-[[7]
- [2]
- [0]
- ...
- [9]
- [0]
- [6]]
-'''
