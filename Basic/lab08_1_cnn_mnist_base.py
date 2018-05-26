c6349983897b69ea72f6da567f614b7874f9f370
Gyeonghoon Lee <azureangma@naver.com>
2018-05-27 00:31:04 +0900

    Update lab08_1_cnn_mnist_base.py

--- Basic/lab08_1_cnn_mnist_base.py
+++ Basic/lab08_1_cnn_mnist_base.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from __future__ import division
 
 import tensorflow as tf
@@ -9,7 +9,8 @@ import time
 
 import load_data
 
-x_train, x_validation, x_test, y_train, y_validation, y_test=load_data.load_mnist('./data/mnist/', seed=0, as_image=True, scaling=True)
+x_train, x_validation, x_test, y_train, y_validation, y_test = load_data.load_mnist('./data/mnist/', seed=0,
+                                                                                    as_image=True, scaling=True)
 
 BOARD_PATH = "./board/lab08-1_board"
 INPUT_DIM = np.size(x_train, 1)
@@ -17,7 +18,6 @@ NCLASS = len(np.unique(y_train))
 BATCH_SIZE = 32
 
 TOTAL_EPOCH = 30
-KEEP_PROB = 0.7
 
 ntrain = len(x_train)
 nvalidation = len(x_validation)
@@ -31,18 +31,17 @@ print("The number of validation samples : ", nvalidation)
 print("The number of test samples : ", ntest)
 
 
-def l1_loss (tensor_op, name='l1_loss'):
+def l1_loss(tensor_op, name='l1_loss'):
     output = tf.reduce_sum(tf.abs(tensor_op), name=name)
     return output
 
 
-def l2_loss (tensor_op, name='l2_loss'):
-    output = tf.reduce_sum(tf.square(tensor_op), name=name)/2
+def l2_loss(tensor_op, name='l2_loss'):
+    output = tf.reduce_sum(tf.square(tensor_op), name=name) / 2
     return output
 
 
 def linear(tensor_op, output_dim, weight_decay=None, regularizer=None, with_W=False, name='linear'):
-
     with tf.variable_scope(name):
         W = tf.get_variable(name='W', shape=[tensor_op.get_shape()[-1], output_dim], dtype=tf.float32,
                             initializer=tf.glorot_uniform_initializer())
@@ -52,9 +51,9 @@ def linear(tensor_op, output_dim, weight_decay=None, regularizer=None, with_W=Fa
 
         if weight_decay:
             if regularizer == 'l1':
-                wd = l1_loss(W)*weight_decay
+                wd = l1_loss(W) * weight_decay
             elif regularizer == 'l2':
-                wd = l2_loss(W)*weight_decay
+                wd = l2_loss(W) * weight_decay
             else:
                 wd = tf.constant(0.)
         else:
@@ -69,14 +68,13 @@ def linear(tensor_op, output_dim, weight_decay=None, regularizer=None, with_W=Fa
 
 
 def relu_layer(tensor_op, output_dim, weight_decay=None, regularizer=None,
-               keep_prob=1.0,is_training=False, with_W=False, name='relu_layer'):
-
+               keep_prob=1.0, is_training=False, with_W=False, name='relu_layer'):
     with tf.variable_scope(name):
         W = tf.get_variable(name='W', shape=[tensor_op.get_shape()[-1], output_dim], dtype=tf.float32,
                             initializer=tf.glorot_uniform_initializer())
         b = tf.get_variable(name='b', shape=[output_dim], dtype=tf.float32,
                             initializer=tf.constant_initializer(0.0))
-        pre_activation = tf.nn.bias_add(tf.matmul(tensor_op, W), b, name = 'pre_op')
+        pre_activation = tf.nn.bias_add(tf.matmul(tensor_op, W), b, name='pre_op')
         bn = tf.contrib.layers.batch_norm(pre_activation,
                                           is_training=is_training,
                                           updates_collections=None)
@@ -85,9 +83,9 @@ def relu_layer(tensor_op, output_dim, weight_decay=None, regularizer=None,
 
         if weight_decay:
             if regularizer == 'l1':
-                wd = l1_loss(W)*weight_decay
+                wd = l1_loss(W) * weight_decay
             elif regularizer == 'l2':
-                wd = l2_loss(W)*weight_decay
+                wd = l2_loss(W) * weight_decay
             else:
                 wd = tf.constant(0.)
         else:
@@ -100,47 +98,47 @@ def relu_layer(tensor_op, output_dim, weight_decay=None, regularizer=None,
         else:
             return dr
 
-        
+
 def conv2d(tensor_op, stride_w, stride_h, shape, name='Conv'):
     with tf.variable_scope(name):
         W = tf.get_variable(name='W', shape=shape, dtype=tf.float32,
                             initializer=tf.truncated_normal_initializer())
         b = tf.get_variable(name='b', shape=shape[-1], dtype=tf.float32,
                             initializer=tf.constant_initializer(0.0))
-        conv = tf.nn.conv2d(tensor_op, W, strides=[1, stride_w, stride_h, 1], padding='SAME', name = 'conv')
-        h = tf.nn.relu(tf.nn.bias_add(conv, b), name = 'h')
+        conv = tf.nn.conv2d(tensor_op, W, strides=[1, stride_w, stride_h, 1], padding='SAME', name='conv')
+        h = tf.nn.relu(tf.nn.bias_add(conv, b), name='h')
     return h
 
 
 def max_pooling(tensor_op, ksize_w, ksize_h, stride_w, stride_h, name='MaxPool'):
     with tf.variable_scope(name):
-        p = tf.nn.max_pool(tensor_op, ksize=[1,ksize_w, ksize_h,1], strides=[1,stride_w, stride_h,1], padding='SAME', name='p')
-    return p    
-                   
-    
+        p = tf.nn.max_pool(tensor_op, ksize=[1, ksize_w, ksize_h, 1], strides=[1, stride_w, stride_h, 1],
+                           padding='SAME', name='p')
+    return p
+
+
 with tf.variable_scope("Inputs"):
-    X = tf.placeholder(shape = [None, image_width, image_height, 1], dtype = tf.float32, name = 'X')
-    Y = tf.placeholder(shape = [None, 1], dtype = tf.int32, name = 'Y')
+    X = tf.placeholder(shape=[None, image_width, image_height, 1], dtype=tf.float32, name='X')
+    Y = tf.placeholder(shape=[None, 1], dtype=tf.int32, name='Y')
     Y_one_hot = tf.reshape(tf.one_hot(Y, NCLASS), [-1, NCLASS], name='Y_one_hot')
 
 h1 = conv2d(X, 1, 1, [5, 5, 1, 32], name='Conv1')
 p1 = max_pooling(h1, 2, 2, 2, 2, name='MaxPool1')
-h2 = conv2d(p1, 1, 1,  [5, 5, 32, 64], name='Conv2')
+h2 = conv2d(p1, 1, 1, [5, 5, 32, 64], name='Conv2')
 p2 = max_pooling(h2, 2, 2, 2, 2, name='MaxPool2')
 h3 = conv2d(p2, 1, 1, [5, 5, 64, 128], name='Conv3')
 p3 = max_pooling(h3, 2, 2, 2, 2, name='MaxPool3')
 
-flat_size = tf.reduce_prod(tf.stack([p3.get_shape()[i] for i in range(1, 4)]))
-conv_flat = tf.reshape(p3, [-1,4*4*128])
-f1 =  relu_layer(conv_flat, 1024, name='FC_Relu')
+f1 = relu_layer(tf.reshape(p3, [-1, 4 * 4 * 128]), 1024, name='FC_Relu')
 logits = linear(f1, NCLASS, name='FC_Linear')
-    
+
 with tf.variable_scope("Optimization"):
     hypothesis = tf.nn.softmax(logits, name='hypothesis')
-    normal_loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=Y_one_hot), name='loss')
+    normal_loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=Y_one_hot),
+                                name='loss')
     weight_decay_loss = tf.get_collection("weight_decay")
-    loss = normal_loss+tf.reduce_sum(weight_decay_loss)
-    optim = tf.train.AdamOptimizer(learning_rate = 0.001).minimize(loss)
+    loss = normal_loss + tf.reduce_sum(weight_decay_loss)
+    optim = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
 
 with tf.variable_scope("Prediction"):
     predict = tf.argmax(hypothesis, axis=1)
@@ -150,7 +148,7 @@ with tf.variable_scope("Accuracy"):
 
 with tf.variable_scope("Summary"):
     avg_train_loss = tf.placeholder(tf.float32)
-    loss_train_avg  = tf.summary.scalar('avg_train_loss', avg_train_loss)
+    loss_train_avg = tf.summary.scalar('avg_train_loss', avg_train_loss)
     avg_train_acc = tf.placeholder(tf.float32)
     acc_train_avg = tf.summary.scalar('avg_train_acc', avg_train_acc)
     avg_validation_loss = tf.placeholder(tf.float32)
@@ -160,7 +158,7 @@ with tf.variable_scope("Summary"):
     merged = tf.summary.merge_all()
 
 init_op = tf.global_variables_initializer()
-total_step = int(ntrain/BATCH_SIZE)
+total_step = int(ntrain / BATCH_SIZE)
 print("Total step : ", total_step)
 with tf.Session() as sess:
     if os.path.exists(BOARD_PATH):
@@ -180,38 +178,37 @@ with tf.Session() as sess:
 
         epoch_start_time = time.perf_counter()
         for step in range(total_step):
-            s = BATCH_SIZE*step
-            t = BATCH_SIZE*(step+1)
-            a, l, _ = sess.run([accuracy, loss, optim], feed_dict={X:x_train[mask[s:t],:], Y:y_train[mask[s:t],:]})
+            s = BATCH_SIZE * step
+            t = BATCH_SIZE * (step + 1)
+            a, l, _ = sess.run([accuracy, loss, optim], feed_dict={X: x_train[mask[s:t], :], Y: y_train[mask[s:t], :]})
             loss_per_epoch += l
             acc_per_epoch += a
         epoch_end_time = time.perf_counter()
         epoch_duration = epoch_end_time - epoch_start_time
-        loss_per_epoch /= total_step*BATCH_SIZE
-        acc_per_epoch /= total_step*BATCH_SIZE
+        loss_per_epoch /= total_step * BATCH_SIZE
+        acc_per_epoch /= total_step * BATCH_SIZE
 
-        va, vl = sess.run([accuracy, loss], feed_dict={X:x_validation, Y:y_validation})
+        va, vl = sess.run([accuracy, loss], feed_dict={X: x_validation, Y: y_validation})
         epoch_valid_acc = va / len(x_validation)
         epoch_valid_loss = vl / len(x_validation)
 
-        s = sess.run(merged, feed_dict = {avg_train_loss:loss_per_epoch, avg_train_acc:acc_per_epoch,
-                                          avg_validation_loss:epoch_valid_loss, avg_validation_acc:epoch_valid_acc})
-        writer.add_summary(s, global_step = epoch)
+        s = sess.run(merged, feed_dict={avg_train_loss: loss_per_epoch, avg_train_acc: acc_per_epoch,
+                                        avg_validation_loss: epoch_valid_loss, avg_validation_acc: epoch_valid_acc})
+        writer.add_summary(s, global_step=epoch)
 
-        if (epoch+1) % 1 == 0:
-
+        if (epoch + 1) % 1 == 0:
             print("Epoch [{:2d}/{:2d}], train loss = {:.6f}, train accuracy = {:.2%}, "
                   "valid loss = {:.6f}, valid accuracy = {:.2%}, duration = {:.6f}(s)"
-                  .format(epoch + 1, TOTAL_EPOCH, loss_per_epoch, acc_per_epoch, epoch_valid_loss, epoch_valid_acc, epoch_duration))
+                  .format(epoch + 1, TOTAL_EPOCH, loss_per_epoch, acc_per_epoch, epoch_valid_loss, epoch_valid_acc,
+                          epoch_duration))
 
     train_end_time = time.perf_counter()
     train_duration = train_end_time - train_start_time
     print("Duration for train : {:.6f}(s)".format(train_duration))
     print("<<< Train Finished >>>")
 
-    ta = sess.run(accuracy, feed_dict = {X:x_test, Y:y_test})
-    print("Test Accraucy : {:.2%}".format(ta/ntest))
-
+    ta = sess.run(accuracy, feed_dict={X: x_test, Y: y_test})
+    print("Test Accraucy : {:.2%}".format(ta / ntest))
 
 '''
 Epoch [ 1/30], train loss = 67.045388, train accuracy = 90.62%, valid loss = 1.202753, valid accuracy = 95.50%, duration = 249.514007(s)
