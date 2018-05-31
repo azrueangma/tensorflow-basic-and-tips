76c272b32028fd82254eba46a6824392c40e9825
Gyeonghoon Lee <azureangma@naver.com>
2018-05-31 11:53:40 +0900

    Update lab08_0_mnist_and_cifar.py

--- Basic/lab08_0_mnist_and_cifar.py
+++ Basic/lab08_0_mnist_and_cifar.py
@@ -1,8 +1,8 @@
 import load_data
-import plot_help
+import plot_helper
 
 x_train, x_validation, x_test, y_train, y_validation, y_test=load_data.load_cifar('./data/cifar/', seed=0, as_image=True, scaling=True)
-plot_help.plot_cifar(x_train, 100)
+plot_helper.plot_cifar(x_train, 100)
 
 x_train, x_validation, x_test, y_train, y_validation, y_test=load_data.load_mnist('./data/cmnist/', seed=0, as_image=True, scaling=True)
-plot_help.plot_mnist(x_train, 100)
+plot_helper.plot_mnist(x_train, 100)
