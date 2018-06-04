067d80fc040cfc28920c51b2796e8bffeaef6eec
Gyeonghoon Lee <azureangma@naver.com>
2018-06-04 17:38:13 +0900

    Add files via upload

---
+++ Basic/lab10-1_model_save.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+import tensorflow as tf
+import numpy as np
+import os
+import load_data
+
+MODEL_DIR = "./model/"
+if not os.path.exists(MODEL_DIR):
+    os.mkdir(MODEL_DIR)
+
+x_train, x_validation, x_test, y_train, y_validation, y_test \
+    = load_data.load_mnist('./data/mnist/', seed=0, as_image=True, scaling=True)
+
+INPUT_DIM = np.size(x_train, 1)
+NCLASS = len(np.unique(y_train))
+BATCH_SIZE = 32
+
+TOTAL_EPOCH = 30
+
+ntrain = len(x_train)
+nvalidation = len(x_validation)
+ntest = len(x_test)
+
+image_width = np.size(x_train, 1)
+image_height = np.size(x_train, 2)
+
+
+def l1_loss(tensor_op, name='l1_loss'):
+    output = tf.reduce_sum(tf.abs(tensor_op), name=name)
+    return output
+
+
+def l2_loss(tensor_op, name='l2_loss'):
+    output = tf.reduce_sum(tf.square(tensor_op), name=name) / 2
+    return output
+
+
+def linear(tensor_op, output_dim, weight_decay=None, regularizer=None, with_W=False, name='linear'):
+    with tf.variable_scope(name):
+        W = tf.get_variable(name='W', shape=[tensor_op.get_shape()[-1], output_dim], dtype=tf.float32,
+                            initializer=tf.glorot_uniform_initializer())
+        b = tf.get_variable(name='b', shape=[output_dim], dtype=tf.float32,
+                            initializer=tf.constant_initializer(0.0))
+        h = tf.nn.bias_add(tf.matmul(tensor_op, W), b, name='h')
+
+        if weight_decay:
+            if regularizer == 'l1':
+                wd = l1_loss(W) * weight_decay
+            elif regularizer == 'l2':
+                wd = l2_loss(W) * weight_decay
+            else:
+                wd = tf.constant(0.)
+        else:
+            wd = tf.constant(0.)
+
+        tf.add_to_collection("weight_decay", wd)
+
+        if with_W:
+            return h, W
+        else:
+            return h
+
+
+def relu_layer(tensor_op, output_dim, weight_decay=None, regularizer=None,
+               keep_prob=1.0, is_training=False, with_W=False, name='relu_layer'):
+    with tf.variable_scope(name):
+        W = tf.get_variable(name='W', shape=[tensor_op.get_shape()[-1], output_dim], dtype=tf.float32,
+                            initializer=tf.glorot_uniform_initializer())
+        b = tf.get_variable(name='b', shape=[output_dim], dtype=tf.float32,
+                            initializer=tf.constant_initializer(0.0))
+        pre_activation = tf.nn.bias_add(tf.matmul(tensor_op, W), b, name='pre_op')
+        bn = tf.contrib.layers.batch_norm(pre_activation,
+                                          is_training=is_training,
+                                          updates_collections=None)
+        h = tf.nn.relu(bn, name='relu_op')
+        dr = tf.nn.dropout(h, keep_prob=keep_prob, name='dropout_op')
+
+        if weight_decay:
+            if regularizer == 'l1':
+                wd = l1_loss(W) * weight_decay
+            elif regularizer == 'l2':
+                wd = l2_loss(W) * weight_decay
+            else:
+                wd = tf.constant(0.)
+        else:
+            wd = tf.constant(0.)
+
+        tf.add_to_collection("weight_decay", wd)
+
+        if with_W:
+            return dr, W
+        else:
+            return dr
+
+
+def conv2d(tensor_op, stride_w, stride_h, shape, name='Conv'):
+    with tf.variable_scope(name):
+        W = tf.get_variable(name='W', shape=shape, dtype=tf.float32,
+                            initializer=tf.truncated_normal_initializer())
+        b = tf.get_variable(name='b', shape=shape[-1], dtype=tf.float32,
+                            initializer=tf.constant_initializer(0.0))
+        conv = tf.nn.conv2d(tensor_op, W, strides=[1, stride_w, stride_h, 1], padding='SAME', name='conv')
+        h = tf.nn.relu(tf.nn.bias_add(conv, b), name='h')
+    return h
+
+
+def max_pooling(tensor_op, ksize_w, ksize_h, stride_w, stride_h, name='MaxPool'):
+    with tf.variable_scope(name):
+        p = tf.nn.max_pool(tensor_op, ksize=[1, ksize_w, ksize_h, 1], strides=[1, stride_w, stride_h, 1],
+                           padding='SAME', name='p')
+    return p
+
+
+with tf.variable_scope("Inputs"):
+    X = tf.placeholder(shape=[None, image_width, image_height, 1], dtype=tf.float32, name='X')
+    Y = tf.placeholder(shape=[None, 1], dtype=tf.int32, name='Y')
+    Y_one_hot = tf.reshape(tf.one_hot(Y, NCLASS), [-1, NCLASS], name='Y_one_hot')
+
+h1 = conv2d(X, 1, 1, [5, 5, 1, 32], name='Conv1')
+p1 = max_pooling(h1, 2, 2, 2, 2, name='MaxPool1')
+h2 = conv2d(p1, 1, 1, [5, 5, 32, 64], name='Conv2')
+p2 = max_pooling(h2, 2, 2, 2, 2, name='MaxPool2')
+h3 = conv2d(p2, 1, 1, [5, 5, 64, 128], name='Conv3')
+p3 = max_pooling(h3, 2, 2, 2, 2, name='MaxPool3')
+
+flat_op = tf.reshape(p3, [-1, 4 * 4 * 128], name = 'flat_op')
+f1 = relu_layer(flat_op, 1024, name='FC_Relu')
+logits = linear(f1, NCLASS, name='FC_Linear')
+
+with tf.variable_scope("Optimization"):
+    hypothesis = tf.nn.softmax(logits, name='hypothesis')
+    normal_loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=Y_one_hot),
+                                name='loss')
+    weight_decay_loss = tf.get_collection("weight_decay")
+    loss = normal_loss + tf.reduce_sum(weight_decay_loss)
+    optim = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+
+with tf.variable_scope("Prediction"):
+    predict = tf.argmax(hypothesis, axis=1)
+
+with tf.variable_scope("Accuracy"):
+    accuracy = tf.reduce_sum(tf.cast(tf.equal(predict, tf.argmax(Y_one_hot, axis=1)), tf.float32))
+
+#create saver
+saver = tf.train.Saver()
+init_op = tf.global_variables_initializer()
+total_step = int(ntrain / BATCH_SIZE)
+with tf.Session() as sess:
+    sess.run(init_op)
+    for epoch in range(TOTAL_EPOCH):
+        loss_per_epoch = 0
+        acc_per_epoch = 0
+
+        np.random.seed(epoch)
+        mask = np.random.permutation(len(x_train))
+        for step in range(total_step):
+            s = BATCH_SIZE * step
+            t = BATCH_SIZE * (step + 1)
+            a, l, _ = sess.run([accuracy, loss, optim], feed_dict={X: x_train[mask[s:t], :], Y: y_train[mask[s:t], :]})
+            loss_per_epoch += l
+            acc_per_epoch += a
+        loss_per_epoch /= total_step * BATCH_SIZE
+        acc_per_epoch /= total_step * BATCH_SIZE
+
+    saver.save(MODEL_DIR+"my_model_{}/model".format(epoch))
