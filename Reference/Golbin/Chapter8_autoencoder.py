cc14b0b648914cfc00064626967364fae598e7b8
Gyeonghoon Lee <azureangma@naver.com>
2018-06-14 12:32:06 +0900

    Upload Chapter8_autoencoder.py

---
+++ Reference/Golbin/Chapter8_autoencoder.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from load_mnist import save_and_load_mnist
+
+dataset = save_and_load_mnist("./data/mnist/")
+
+x_train = dataset['train_data']
+y_train = dataset['train_target']
+x_test = dataset['test_data']
+y_test = dataset['test_target']
+
+global_step = tf.Variable(0, trainable=False, name='global_step')
+
+X = tf.placeholder(dtype=tf.float32, shape=[None, 784], name='X')
+
+W_encode = tf.get_variable(name='W_encode', shape=[784, 256], initializer=tf.glorot_uniform_initializer())
+b_encode = tf.get_variable(name='b_encode', shape=[256], initializer=tf.zeros_initializer())
+encoder = tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(X, W_encode), b_encode), name='encoder')
+
+W_decode = tf.get_variable(name='W_decode', shape=[256, 784], initializer=tf.glorot_uniform_initializer())
+b_decode = tf.get_variable(name='b_decode', shape=[784], initializer=tf.zeros_initializer())
+decoder = tf.nn.sigmoid(tf.nn.bias_add(tf.matmul(encoder, W_decode), b_decode), name='decoder')
+
+cost = tf.reduce_mean(tf.square(X-decoder), name='cost')
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+train_op = optimizer.minimize(cost, global_step=global_step)
+
+
+total_epochs = 1
+batch_size = 32
+total_steps = int(len(x_train)/batch_size)
+print(">>> Training Start [total epochs : {}, total step : {}]".format(total_epochs, total_steps))
+with tf.Session() as sess:
+    saver = tf.train.Saver(tf.global_variables())
+    ckpt = tf.train.get_checkpoint_state('./model8')
+    if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+        saver.restore(sess, ckpt.model_checkpoint_path)
+    else:
+        sess.run(tf.global_variables_initializer())
+
+    for epoch in range(total_epochs):
+        loss_per_epoch = 0
+
+        np.random.seed(epoch)
+        mask = np.random.permutation(len(x_train))
+        for step in range(total_steps):
+            s = step*batch_size
+            t = (step+1)*batch_size
+            c, _= sess.run([cost, train_op], feed_dict={X:x_train[mask[s:t]]})
+
+            loss_per_epoch += c/total_steps
+
+        print("Global Step : {:5d}, Epoch : [{:3d}/{:3d}], cost : {:.6f}"
+              .format(sess.run(global_step), epoch, total_epochs, loss_per_epoch))
+
+        saver.save(sess, "./model8/dnn.ckpt", global_step=global_step)
+
+
+    #임의로 10개만 택하여 확인해본다.
+    sample_size = 10
+    np.random.seed(0)
+    mask = np.random.choice(a=len(x_test), size=10, replace=False)
+    samples = sess.run(decoder, feed_dict={X:x_test[mask]})
+
+    fig, ax = plt.subplots(2, sample_size, figsize=(sample_size, 2))
+
+    for i in range(sample_size):
+        ax[0][i].set_axis_off()
+        ax[1][i].set_axis_off()
+        ax[0][i].imshow(np.reshape(x_test[mask[i]], (28, 28)), cmap=plt.cm.gray_r)
+        ax[1][i].imshow(np.reshape(samples[i], (28, 28)), cmap=plt.cm.gray_r)
+
+    plt.show()
