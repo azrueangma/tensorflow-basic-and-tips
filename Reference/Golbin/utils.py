e0e7b1b11825043b9159a244c458e4edff0a8df9
Gyeonghoon Lee <azureangma@naver.com>
2018-06-15 18:22:19 +0900

    Upload  utils

---
+++ Reference/Golbin/utils.py
@@ -0,0 +1,13 @@
+import functools
+import tensorflow as tf
+
+def lazy_property(function):
+    attribute='_'+function.__name__
+    @property
+    @functools.wraps(function)
+    def wrapper(self):
+        if not hasattr(self, attribute):
+            with tf.variable_scope(function.__name__):
+                setattr(self, attribute, function(self))
+        return getattr(self, attribute)
+    return wrapper
